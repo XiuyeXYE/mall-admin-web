5d4f64d99b186caf09640956f4d8bc2015805a45
xiuye <xiuye_engineer@outlook.com>
2020-08-03 23:06:10 +0800

    config

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8080"'
+  // BASE_API: '"http://localhost:8086"'
+  BASE_API: '"http://106.13.163.111:8086"',
+
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"http://120.27.63.9:8080"'
+  // BASE_API: '"http://120.27.63.9:8086"'
+  BASE_API: '"http://106.13.163.111:8086"'
 }
